f239f660810e03a5b669b1223eab0c9660b1c279
Thomagotchi <thomassano87@gmail.com>
2025-05-09 13:39:39 +0200

    Update - JS - Pages - Photographer - Added logic to fetch corresponding photographer by url

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,36 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import { getPhotographerMedias, getPhotographers } from "../Api/api.js";
+import { photographerTemplate } from "../templates/photographer.js";
+import { initMediaModal } from "../utils/mediaModal.js";
+
+const getPhotographerID = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("id");
+};
+
+const currentPhotographer = async () => {
+  const photographers = await getPhotographers();
+  const photographerID = getPhotographerID();
+  const photographer = photographers.find(
+    (photographer) => photographer.id === parseInt(photographerID)
+  );
+  return photographer;
+};
+
+async function init() {
+  const photographer = await currentPhotographer();
+  const medias = await getPhotographerMedias(photographer.id);
+
+  if (photographer) {
+    const contactModalName = document.querySelector(".contact_modal_name");
+    contactModalName.textContent = photographer.name;
+    const photographerModel = photographerTemplate(photographer);
+    photographerModel.getPhotographerHeadDOM(photographer);
+    photographerModel.getPhotographerGalleryDOM(photographer, medias);
+    photographerModel.getPhotographerStickyDOM(photographer, medias);
+  } else {
+    console.error("Photographer not found");
+  }
+}
+
+init();
+initMediaModal();
