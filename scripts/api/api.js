65def50f0fe2230e6d7a19aec8b95ff65318b019
Thomagotchi <thomassano87@gmail.com>
2025-05-27 21:03:06 +0200

    Update - API - JS - Added comments to project

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -1,23 +1,28 @@
+// Récupération des photographes
 export async function getPhotographers() {
   const response = await fetch("../../data/photographers.json");
   const json = await response.json();
   return json.photographers;
 }
 
+// Récupération des médias
 export async function getMedias() {
   const response = await fetch("../../data/photographers.json");
   const json = await response.json();
   return json.media;
 }
 
+// Récupération du photographe
 export async function getPhotographer(id) {
   const photographers = await getPhotographers();
   return photographers.find((photographer) => photographer.id === parseInt(id));
 }
 
+// Récupération des médias du photographe
 export async function getPhotographerMedias(id) {
-  const photographer = await getPhotographer(id);
   const medias = await getMedias();
-
-  return medias.filter((media) => media.photographerId === photographer.id);
+  const filteredMedias = medias.filter(
+    (media) => media.photographerId === parseInt(id)
+  );
+  return filteredMedias;
 }
